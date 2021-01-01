2d2cb4e0fc508369d44441affe65f94d16cba9de
AnthonyJo <joseongkwang@gmail.com>
2021-01-01 20:29:17 +0900

    project init

---
+++ HelloSwift/HelloSwift.swift
@@ -0,0 +1,14 @@
+import Swift 
+
+/*     let a: Int = 100  //value
+    var b: Int = 200  //variable
+
+//    a = 300
+    b = 400 */
+
+  print("hello")
+
+/*   print("a = \(a)")
+  print("b = \(b)") */
+
+
---
+++ Variable/Variable.swift
@@ -0,0 +1,6 @@
+let a = 100
+var b = 200
+
+b = 300
+print("a = \(a)")
+print("b = \(b)")
